ed889f6c213258a4d96794f33bbb68397568d979
VulpineReynard <milare8@gmail.com>
2019-07-17 15:25:09 -0400

    setup SASS and receiving data from NASA API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [nasaData, setNasaData] = useState("https://api.nasa.gov/planetary/apod?api_key=AcOjW5HR3dXgctkKQ3hfCLxIs8yyy26t02u9RKkc")
+
+  axios.get(nasaData)
+    .then(response => {
+      console.log(response);
+    })
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+
     </div>
   );
 }
